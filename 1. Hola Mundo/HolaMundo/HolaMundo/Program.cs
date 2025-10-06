77f87b295c1898874fc430f70fe67e21cd862794
SugusGamberra <125316695+SugusGamberra@users.noreply.github.com>
2025-10-06 16:25:58 +0200

    Add files via upload

---
+++ 1. Hola Mundo/HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,28 @@
+﻿// "Hello World"
+// C# se parece a JAVA
+// En C# mostramos por consola con System.Console.WriteLine
+
+// System.Console.WriteLine("Hola Mundo");
+
+// Si queremos usar WriteLine sin el prefijo System lo hacemos de la siguiente forma:
+
+using System;
+
+namespace HolaMundo
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Este es nuestro primer programa en C#");
+        }
+    }
+}
+
+// namespace y class agrupan el código
+// static indica que el método Main pertenece a la clase y no a una instancia de la clase
+// es el punto de entrada del programa
+
+// para probar un método u otro recuerda descomentar el que vayas a usar y comentar el que no uses con // al inicio de la línea
+// para ejecutar el programa en la terminal escribe dotnet run
