c395c23cd449e6a2e9b543b63db3df3d8da6a452
SugusGamberra <125316695+SugusGamberra@users.noreply.github.com>
2025-10-08 20:18:51 +0200

    Add files via upload
    Añadido calculo area triangulo

---
+++ 2. Area Triangulo/AreaTriangulo/AreaTriangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// Calculamos el área de un triángulo usando doubles  
+// BTW base en C# es una palabra reservada, por eso usamos baseTriangulo
+
+namespace AreaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseTriangulo = 10.5;
+            double altura = 5.5;
+            double area = (baseTriangulo * altura) / 2;
+
+            Console.WriteLine("El área del triángulo es: " + area);
+        }
+
+    }
+
+}
