a3fdde043da62220e4b4158b8081e34af1074d18
SugusGamberra <125316695+SugusGamberra@users.noreply.github.com>
2025-10-15 08:51:27 +0200

    Añadiendo ecuaciones de 2 grado

--- 2. Area Triangulo/AreaTriangulo/AreaTriangulo/Program.cs
+++ 2. Area Triangulo/AreaTriangulo/AreaTriangulo/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 
 // Calculamos el área de un triángulo usando doubles  
@@ -18,4 +19,26 @@ namespace AreaTriangulo
 
     }
 
+=======
+﻿using System;
+
+// Calculamos el área de un triángulo usando doubles  
+// BTW base en C# es una palabra reservada, por eso usamos baseTriangulo
+
+namespace AreaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseTriangulo = 10.5;
+            double altura = 5.5;
+            double area = (baseTriangulo * altura) / 2;
+
+            Console.WriteLine("El área del triángulo es: " + area);
+        }
+
+    }
+
+>>>>>>> c2ddebb (Añadiendo ecuaciones de 2 grado)
 }
---
+++ 3. EcuacionSegundoGrado/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Vamos a calcular ecuaciones de segundo grado
+
+using System;
+
+namespace EcuacionesSegundoGradoSimple
+
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos a calcular ecuaciones de segundo grado");
+
+            Console.WriteLine("Introduce el valor de a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Introduce el valor de b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Introduce el valor de c:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double discriminante = b * b - 4 * a * c;
+            if (discriminante > 0)
+            {
+                double x1 = (-b + Math.Sqrt(discriminante)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(discriminante)) / (2 * a);
+                Console.WriteLine($"Las soluciones son reales y diferentes: x1 = {x1}, x2 = {x2}");
+            }
+            else if (discriminante == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine($"Las soluciones son reales e iguales: x = {x}");
+            }
+            else
+            {
+                Console.WriteLine("Las soluciones son complejas y no reales.");
+
+                Console.WriteLine("\nPulsa una tecla para finalizar...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
